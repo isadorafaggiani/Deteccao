9f4c7ee4d6d53013599fb6a1706ec105b4b2f15a
isadorafaggiani <isadora.cfaggiani@gmail.com>
2021-11-03 13:39:55 -0300

    add full screen and images.

--- calibragem.py
+++ calibragem.py
@@ -43,6 +43,10 @@ x_max = 0
 y_min = 0
 y_max = 0
 
+window_name = "Gaze Tracking"
+cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+cv2.moveWindow(window_name, 0, 0)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 def calibragem():
     print("entrou na função calibragem")
@@ -54,8 +58,11 @@ def calibragem():
     gaze = GazeTracking()
     webcam = cv2.VideoCapture(0)
 
-    SCREEN_WIDTH = webcam.get(cv2.CAP_PROP_FRAME_WIDTH)
-    SCREEN_HEIGHT = webcam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    SCREEN_WIDTH = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    SCREEN_HEIGHT = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print(f'SCREEN_WIDTH = {SCREEN_WIDTH}, SCREEN_HEIGHT = {SCREEN_HEIGHT}')
+    pass
+    blank_image = np.zeros((SCREEN_HEIGHT,SCREEN_WIDTH,3), dtype=np.uint8)+255
 
     while True:
         # We get a new frame from the webcam
@@ -72,19 +79,19 @@ def calibragem():
         res = ini - temp_ini  # diferença do tempo inicial e o tempo dentro do while
         time_results.append(res)  # colocar res no vetor
 
-        # text = ""
-        # if gaze.is_blinking():
-        #     text = "Blinking"
-        # elif gaze.is_right():
-        #     text = "Looking right"
-        #     # contadorRight += 1
-        # elif gaze.is_left():
-        #     text = "Looking left"
-        #     # contadorLeft += 1
-        # elif gaze.is_center():
-        #     text = "Looking center"
+        text = ""
+        if gaze.is_blinking():
+            text = "Blinking"
+        elif gaze.is_right():
+            text = "Looking right"
+            # contadorRight += 1
+        elif gaze.is_left():
+            text = "Looking left"
+            # contadorLeft += 1
+        elif gaze.is_center():
+            text = "Looking center"
 
-        #cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        #cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (255, 0, 0), 2)
         gaze_x = gaze.horizontal_ratio()
         gaze_y = gaze.vertical_ratio()
 
@@ -93,38 +100,32 @@ def calibragem():
             contador = contador + 100
             if gaze_x != None and gaze_y != None:
 
-                # +-------------+
+                # +------X------+
                 # |             |
+                # X             X
                 # |             |
-                # |             |
-                # +-------------+
+                # +------X------+
 
+                pos = ()
                 if contador < 3000:
-                    cv2.putText(frame, "*", (0, int(SCREEN_HEIGHT)), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31),
-                                1)  # esq
-
+                    pos = "esquerda"
                 elif 3000 <= contador < 6000:
-                    print("entrou")
-                    cv2.putText(frame, "*", (int(5), int(20)), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)  # cima
-
+                    pos = "cima"
                 elif 6000 <= contador < 9000:
-                    cv2.putText(frame, "*", (int(SCREEN_WIDTH - 15), int(20)), cv2.FONT_HERSHEY_DUPLEX, 0.9,
-                                (147, 58, 31), 1)  # di
-
+                    pos = "direita"
                 elif 9000 <= contador < 12000:
-                    cv2.putText(frame, "*", (int(SCREEN_WIDTH - 17), int(SCREEN_HEIGHT)), cv2.FONT_HERSHEY_DUPLEX, 0.9,
-                                (147, 58, 31), 1)  # baixo
+                    pos = "baixo"
                 else:
                     break
+
+                cv2.imshow(window_name, cv2.imread(f'img_{pos}.png'))
 
                 lista_gaze_x.append(gaze_x)
                 lista_gaze_y.append(gaze_y)
 
-                # print(f'x = {gaze_x:.3f} (min = {menor_x:.3f}, max = {maior_x:.3f})\ty = {gaze_y:.3f}')
-                print(contador)
                 coords.append([pd.Timedelta(milliseconds=contador), gaze_x, gaze_y])
 
-        cv2.imshow("Demo", frame) and elapsed_time > duration
+        #cv2.imshow("Demo", cv2.imread(path)) and elapsed_time > duration
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print(f'Main loop finished after {str(int(elapsed_time))} seconds.')
             break
@@ -133,9 +134,13 @@ def calibragem():
     lista_gaze_x.append(lista_gaze_x.sort())
     lista_gaze_x.pop()
 
-    # pegar 10 primeiros numeros
+    # ordenar os y de forma crescente
+    lista_gaze_y.append(lista_gaze_y.sort())
+    lista_gaze_y.pop()
+
+    # pegar 10 primeiros x
     menores_x = lista_gaze_x[:10]
-    # pegar 10 ultimos numeros
+    # pegar 10 ultimos x
     maiores_x = lista_gaze_x[-10:]
 
     # x_min = media dos 10 primeiros gaze_x
@@ -145,20 +150,16 @@ def calibragem():
     x_max = sum(maiores_x) / len(maiores_x)
     print('X max ', x_max)
 
-    # print (lista_gaze_x)
-
-    # ordenar os y de forma crescente
-    lista_gaze_y.append(lista_gaze_y.sort())
-    lista_gaze_y.pop()
-
-    # y_min = media dos 10 primeiros gaze_y
+    # pegar 10 primeiros y
     menores_y = lista_gaze_y[:10]
-    # y_max = media dos 10 ultimos gaze_y
+    # pegar 10 ultimos y
     maiores_y = lista_gaze_y[-10:]
 
+    # y_min = media dos 10 primeiros gaze_y
     y_min = sum(menores_y) / len(menores_y)
     print('Y min ', y_min)
-    # x_max = media dos 10 ultimos gaze_x
+
+    # y_max = media dos 10 ultimos gaze_y
     y_max = sum(maiores_y) / len(maiores_y)
     print('Y max ', y_max)
 
