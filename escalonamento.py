ea18fd1211da28efb1cfb25be0fa9934cad66e6f
isadorafaggiani <isadora.cfaggiani@gmail.com>
2021-11-16 22:59:53 -0300

    add the process for y coordinates at escalonamento.py

--- escalonamento.py
+++ escalonamento.py
@@ -4,9 +4,13 @@ def escalonamento(escala, coords):
 
 	x_min, x_max, y_min, y_max = escala[0], escala[1], escala[2], escala[3]
 	data_x = coords['gaze_x']
-	scaler = MinMaxScaler(feature_range = (x_min, x_max))
+	scaler = MinMaxScaler(feature_range=(x_min, x_max))
 	data_x = scaler.fit_transform(data_x)
+
 	# repetir o processo para y
+	data_y = coords['gaze_y']
+	scaler = MinMaxScaler(feature_range=(y_min, y_max))
+	data_y = scaler.fit_transform(data_y)
 
 	# repetir oS processoS para o outro olho
 
