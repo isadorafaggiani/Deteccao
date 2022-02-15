23ce5461ff68ff42b3a1bd813d2cd13cc0d37a20
Marcelo Cysneiros <marcelovca90@gmail.com>
2022-02-15 12:15:53 -0300

    Add argument to load from cache

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import calibragem, captura, escalonamento, exibicao
 from pathlib import Path
 import json
+import sys
 
-CACHE_CALIBRAGEM = 'cache/calibragem.json'
-CACHE_CAPTURA = 'cache/captura.json'
+CACHE_CALIBRAGEM = './cache/calibragem.json'
+CACHE_CAPTURA = './cache/captura.json'
+CACHE_ESCALONAMENTO = './cache/escalonamento.json'
 
 if __name__ == '__main__':
 
 	# calibragem
-	if Path(CACHE_CALIBRAGEM).exists():
-		print('Obtendo calibragem do cache...')
+	if ('-c' in sys.argv or '--cache' in sys.argv) and Path(CACHE_CALIBRAGEM).exists():
+		print('Tentando obter calibragem do cache...')
 		with open(CACHE_CALIBRAGEM, 'r') as f:
 			resultado_calibragem = json.load(f)
 	else:
-		print('Realizando calibragem...')
+		print('Realizando nova calibragem...')
 		resultado_calibragem = calibragem.calibrar()
 	escala_webcam = resultado_calibragem[:4]
 	resolucao_tela = resultado_calibragem[4:]
@@ -21,19 +23,26 @@ if __name__ == '__main__':
 	print(f'resolucao_tela = {resolucao_tela}')
 
 	# captura
-	if Path(CACHE_CAPTURA).exists():
-		print('Obtendo captura do cache...')
+	if ('-c' in sys.argv or '--cache' in sys.argv) and Path(CACHE_CAPTURA).exists():
+		print('Tentando obter captura do cache...')
 		with open(CACHE_CAPTURA, 'r') as f:
 			coords = json.load(f)
 	else:
+		print('Realizando nova captura...')
 		coords = captura.capturar()
 	print(f'coords(cap) = {coords}')
 
 	# escalonamento
-	coords = escalonamento.escalonar(escala_webcam, coords)
-	print(f'coords(esc) = {coords}')
-	coords = escalonamento.escalonar(resolucao_tela, coords)
-	print(f'coords(tela) = {coords}')
+	if ('-c' in sys.argv or '--cache' in sys.argv) and Path(CACHE_ESCALONAMENTO).exists():
+		print('Tentando obter escalonamento do cache...')
+		with open(CACHE_ESCALONAMENTO, 'r') as f:
+			coords = json.load(f)
+	else:
+		print('Realizando novo escalonamento...')
+		coords = escalonamento.escalonar(escala_webcam, coords)
+		print(f'coords(esc) = {coords}')
+		coords = escalonamento.escalonar(resolucao_tela, coords)
+		print(f'coords(tela) = {coords}')
 	
 	# exibicao
 	exibicao.exibir(coords)
